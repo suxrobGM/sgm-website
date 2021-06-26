b2b8b85198e467c0ca8eda48744bad169867f5bc
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2021-06-26 20:50:14 +0800

    integrated Serilog, updated nuget packages

--- src/SGM.Web.Blog/Program.cs
+++ src/SGM.Web.Blog/Program.cs
@@ -1,12 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 using SGM.Infrastructure.Data;
 
 namespace SGM.Web.Blog
@@ -15,20 +12,61 @@ namespace SGM.Web.Blog
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
+            var configuration = BuildConfiguration();
+            Log.Logger = CreateLogger(configuration);
 
-            using var scope = host.Services.CreateScope();
-            var serviceProvider = scope.ServiceProvider;
-            SeedData.Initialize(serviceProvider);
+            try
+            {
+                Log.Logger?.Information("-------------------------------------------------");
+                Log.Logger?.Information("Started webapp SGM Blog");
+                
+                var host = CreateHostBuilder(args).Build();
+                using var scope = host.Services.CreateScope();
+                var serviceProvider = scope.ServiceProvider;
+                SeedData.Initialize(serviceProvider);
 
-            host.Run();
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger?.Fatal(ex, "Application start-up failed");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        
+        #region Static methods for creating logger 
+
+        private static IConfiguration BuildConfiguration()
+        {
+            Environment.SetEnvironmentVariable("BASEDIR", AppContext.BaseDirectory);
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            return new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true)
+                .Build();
+        }
+
+        private static ILogger CreateLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        #endregion
     }
 }
--- src/SGM.Web/Program.cs
+++ src/SGM.Web/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace SGM.Web
 {
@@ -7,14 +10,55 @@ namespace SGM.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = BuildConfiguration();
+            Log.Logger = CreateLogger(configuration);
+
+            try
+            {
+                Log.Logger?.Information("-------------------------------------------------");
+                Log.Logger?.Information("Started webapp SGM Profile");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger?.Fatal(ex, "Application start-up failed");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        
+        #region Static methods for creating logger 
+
+        private static IConfiguration BuildConfiguration()
+        {
+            Environment.SetEnvironmentVariable("BASEDIR", AppContext.BaseDirectory);
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            return new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true)
+                .Build();
+        }
+
+        private static ILogger CreateLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        #endregion
     }
 }
